1776c926c1e74cce47860f8573c800dada625ce2
piotrekp <piotrekpa90@gmail.com>
2011-11-24 22:41:06 +0100

    Połączenie z bazą danych

--- src/dbController.java
+++ src/dbController.java
@@ -1,16 +1,44 @@
 import java.sql.Connection;
-
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class dbController {
 
-	Connection conn = null;
-	public static String userName, password, host;
-	
-	public static void dbController() {
+	public static String userName, password, host, dbname;
+	public static Connection conn = null;
+
+	public dbController() {
 		
-		userName = "";
+		/*
+		 * Dane konfiguracyjne bazy danych MySQL
+		 */
+
+		userName = "root";
 		password = "";
-		host = "";
+		host = "jdbc:mysql://localhost:3306/";
+		dbname = "naukajazdy";
 		
+		dbController.connect();
+				
+	}
+	
+	public static Connection connect() {
+		
+
+		/*
+		 * üˆczenie z bazˆ danych.
+		 */
+
+		try {
+			conn = DriverManager.getConnection(host + dbname, userName, password);
+			return conn;
+		}
+
+		catch (SQLException e) {
+			System.out
+					.println("Napotkano b¸ˆd podczas ustanawiania po¸ˆczenia z bazˆ danych: "
+							+ e.getMessage());
+			return null;
+		}
 	}
 }
