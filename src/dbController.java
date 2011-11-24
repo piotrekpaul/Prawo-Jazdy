c29d31f2c3733164dc3d3b0cf5f2349ff8e85e1e
piotrekp <piotrekpa90@gmail.com>
2011-11-24 23:18:05 +0100

    dbController tuneup
    Zaczyna działać. Statyczna konstrukcja, beta SELECT'a.

--- src/dbController.java
+++ src/dbController.java
@@ -1,14 +1,17 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class dbController {
 
 	public static String userName, password, host, dbname;
-	public static Connection conn = null;
+	static Connection conn = null;
+	static Statement query = null;
 
 	public dbController() {
-		
+
 		/*
 		 * Dane konfiguracyjne bazy danych MySQL
 		 */
@@ -17,28 +20,54 @@ public class dbController {
 		password = "";
 		host = "jdbc:mysql://localhost:3306/";
 		dbname = "naukajazdy";
-		
-		dbController.connect();
+
+		if (this.connect() != null) {
+
+			try {
 				
+				query = conn.createStatement();
+			} 
+			
+			catch (SQLException e) {
+				
+				System.out.println("Napotkano blad podczas tworzenia zapytania: "+ e.getMessage());
+			}
+		}
+
 	}
-	
-	public static Connection connect() {
-		
+
+	private Connection connect() {
 
 		/*
 		 * üˆczenie z bazˆ danych.
 		 */
 
 		try {
-			conn = DriverManager.getConnection(host + dbname, userName, password);
+			
+			conn = DriverManager.getConnection(host + dbname, userName,password);
 			return conn;
 		}
 
 		catch (SQLException e) {
-			System.out
-					.println("Napotkano b¸ˆd podczas ustanawiania po¸ˆczenia z bazˆ danych: "
-							+ e.getMessage());
+			
+			System.out.println("Napotkano blad podczas ustanawiania polaczenia z baza danych: "+ e.getMessage());
 			return null;
 		}
+	}
+
+	public ResultSet getData (String what, String fromWhere) throws SQLException {
+		ResultSet result = query.executeQuery("SELECT " + what + " from " + fromWhere);
+		return result;
+	}
+
+	public static void main(String[] args) throws SQLException {
+	
+		dbController db = new dbController();
+		
+		while(db.getData("*", "users").next()) {
+			System.out.println("Znaleziono rekord!");
+		}
+		
+		
 	}
 }
